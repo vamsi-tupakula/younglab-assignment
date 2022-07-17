ed06ddb94d1d650c0bcf5105b1679819fd8dae67
Vamsi <vamsitupakula474@gmail.com>
2022-07-17 19:36:52 +0530

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,16 @@ import React from 'react'
 import './css/Navbar.css'
 
 function Navbar() {
-  return (
-    <div>Navbar</div>
-  )
+    return (
+        <div className='navbar'>
+            <div className="logo">YourChallenge</div>
+            <div className="links">
+                <a href="/" className='product active-link'>Product</a>
+                <a href="/" className='download'>Download</a>
+                <a href="/" className='pricing'>Pricing</a>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
