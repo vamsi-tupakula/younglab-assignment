10162e4e7bb9d6eeaafe6c4724621b9c32d44a5d
Vamsi <vamsitupakula474@gmail.com>
2022-07-17 19:43:24 +0530

    major card done

--- src/components/MajorCard.jsx
+++ src/components/MajorCard.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './css/MajorCard.css'
 
-function MajorCard() {
+function MajorCard(props) {
+  const majorData = props.result;
+
   return (
-    <div>MajorCard</div>
+    <div className='major-card'>
+      <div className="img">
+        <img className='image' src={majorData.picture.large} alt="profile" />
+      </div>
+      <div className="text">
+        <h1>{majorData.name.title} {majorData.name.first} {majorData.name.last}</h1>
+        <p className='address'><span className='street_no'>{majorData.location.street.number}, </span>{majorData.location.street.name}, {majorData.location.city}, {majorData.location.state}, <b>{majorData.location.country}</b>, {majorData.location.postcode}</p>
+        <p className='timezone'>{majorData.location.timezone.offset} - {majorData.location.timezone.description}</p>
+        <p className='gender'>{majorData.gender.slice(0,1).toUpperCase()}{majorData.gender.slice(1)}</p>
+      </div>
+    </div>
   )
 }
 
