6042ff0944b62b16ff3d99196dd0e641d5e60130
Vamsi <vamsitupakula474@gmail.com>
2022-07-17 19:45:29 +0530

    minor cards done

--- src/components/MinorCards.jsx
+++ src/components/MinorCards.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './css/MinorCards.css'
 
-function MinorCards() {
+function MinorCard(props) {
+  const results = props.result.results;
+  const [currentCard, setCurrentCard] = useState('0');
+  const [prevCard, setPrevCard] = useState('0');
+  // console.table(results);
+  
+  const changeCard = (elem,index) => {
+   props.setMajorData(elem);
+   setPrevCard(currentCard);
+   setCurrentCard(index);
+  }
+
+  useEffect(() => {
+    document.getElementById(prevCard).classList.remove('active');
+    document.getElementById(currentCard).classList.add('active');
+  }, [currentCard,prevCard])
+  
+
   return (
-    <div>MinorCards</div>
+    <div className="grid">
+      {results.map((elem,index) => {
+        return (
+        <div className='card' id={index} key={index} onClick={() => changeCard(elem, index)}>
+          <p className='gender'>{elem.gender.slice(0,1).toUpperCase()}{elem.gender.slice(1)} • {elem.nat}</p>
+          <p className='name'>{elem.name.title} {elem.name.first} {elem.name.last}</p>
+          <p className='email'>{elem.email}</p>
+        </div>)
+      })}
+    </div>
   )
 }
 
-export default MinorCards
+export default MinorCard
